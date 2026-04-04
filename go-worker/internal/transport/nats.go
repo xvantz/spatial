07dacb20db6e5b675ac56f9c97609c80132d7124
Ivan R. <xvantz@gmail.com>
2026-04-04 16:10:01 +0700

    chore: add comments to nats connection

--- go-worker/internal/transport/nats.go
+++ go-worker/internal/transport/nats.go
@@ -1,3 +1,4 @@
+// Package transport handles the messaging infrastructure using NATS.
 package transport
 
 import (
@@ -8,17 +9,19 @@ import (
 	"github.com/nats-io/nats.go"
 )
 
+// Broker manages the connection to the NATS message bus.
 type Broker struct {
 	nc *nats.Conn
 }
 
+// NewBroker initializes a new connection to the NATS server.
 func NewBroker(url string) (*Broker, error) {
 	nc, err := nats.Connect(url,
 		nats.MaxReconnects(-1),
 		nats.ReconnectWait(2*time.Second),
 		nats.PingInterval(20*time.Second),
 		nats.MaxPingsOutstanding(3),
-		nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+		nats.DisconnectErrHandler(func(_ *nats.Conn, err error) {
 			log.Printf("[NATS] Disconnected: %v", err)
 		}),
 		nats.ReconnectHandler(func(nc *nats.Conn) {
@@ -35,14 +38,17 @@ func NewBroker(url string) (*Broker, error) {
 	}, nil
 }
 
+// Publish sends a message to the specified subject.
 func (b *Broker) Publish(subject string, data []byte) error {
 	return b.nc.Publish(subject, data)
 }
 
+// Subscribe registers a listener for the specified subject.
 func (b *Broker) Subscribe(subject string, handler func(msg *nats.Msg)) (*nats.Subscription, error) {
 	return b.nc.Subscribe(subject, handler)
 }
 
+// Shutdown gracefully closes the NATS connection.
 func (b *Broker) Shutdown() {
 	log.Println("[NATS] Start drain...")
 	if err := b.nc.Drain(); err != nil {
